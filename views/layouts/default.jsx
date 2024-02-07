ca4332ff381ec9078f7ea8c423f10fe8a21c3632
Vanessa Kasminoff <sunnset07@gmail.com>
2024-02-06 22:37:15 -0800

    Added css file to style images

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -5,6 +5,7 @@ const Default = (html) => {
         <html>
             <head>
                 <title>Rest-RANT</title>
+                <link rel='stylesheet' href='/css/main.css' />
             </head>
             <body>
                 <div className='container'>{html.children}</div>
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -6,7 +6,7 @@ const index = ({places}) => {
         <Default>
                 <h1>PLACES INDEX PAGE</h1>
                 {places.map((place) => (
-                    <div key={place.name}>
+                    <div key={place.name} className='places-div'>
                     <h2 key={place.name}>{place.name}</h2>
                     <img src={place.pic} alt={place.name} key={place.name}/>
                     </div>
