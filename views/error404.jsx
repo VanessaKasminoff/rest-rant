41154bc3270f8c8b278a07cf08ccb2fe88fb7630
Vanessa Kasminoff <sunnset07@gmail.com>
2024-02-07 20:06:21 -0800

    Added homepage image, edited the 404 page text, and added a favicon

--- views/error404.jsx
+++ views/error404.jsx
@@ -9,7 +9,7 @@ const error404 = () => {
                 <p className='p-404'>*sniff* *sniff*</p>
                 <img src='https://images.unsplash.com/photo-1582902412163-ea8eeb98b623?q=80&w=1304&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='Dog looking closely to screen' />
                 <p>Photo by <a href="https://unsplash.com/@indrajeetwashere?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Indrajeet Choudhary</a> on <a href="https://unsplash.com/photos/brown-short-coated-dog-on-brown-dried-leaves-fOEX_2JLmBI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></p>
-                <p className='p-404'>Looks like you stumbled on the wrong page.</p>
+                <p className='p-404'>Looks like the page you wanted is not here!</p>
             </main>
         </Default>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,9 @@ const home = () => {
         <Default>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src='/images/rest-rant-home.jpg' alt='Hands holding ipad leaving food reviews'/>
+                </div>
                 <a href='/places'>
                     <button className='btn btn-primary'>Places Page</button>
                 </a>
