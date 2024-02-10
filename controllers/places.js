13e8e85047594e818e30aabc6dccb146fd394345
Vanessa Kasminoff <sunnset07@gmail.com>
2024-02-10 11:58:14 -0800

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -29,6 +29,31 @@ router.get('/:id', (req, res) => {
     }
 })
 
+//Update edit
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.status(404).send(render('error404'))
+    }
+    else if (!Place[id]) {
+        res.status(404).send(render('error404'))
+    }
+    else {
+        if (!req.body.pic) {
+            req.body.pic = 'https://images.unsplash.com/photo-1552566626-52f8b828add9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Some City'
+        }
+        if (!req.body.country) {
+            req.body.country = 'Some Country'
+        }
+        Place[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+  })
+  
+
 //create
 router.post('/', (req, res) => {
     if (!req.body.pic) {
