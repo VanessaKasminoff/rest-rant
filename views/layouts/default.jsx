3e530f30d91a02ac9a8e10ae97872761b6ab0cd9
Vanessa Kasminoff <sunnset07@gmail.com>
2024-02-10 00:56:11 -0800

    Added footer and increased top margin of carousel div

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -27,6 +27,9 @@ const Default = (html) => {
                     </ul>
                 </nav>
                 <div className='container'>{html.children}</div>
+                <footer>
+                    <p>Rest-RANT &copy; 2024</p>
+                </footer>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" 
                 integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" 
                 crossOrigin="anonymous"></script>
