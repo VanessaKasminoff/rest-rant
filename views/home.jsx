bad106a1292d834ee79bf85e30dc0771d5979af3
Vanessa Kasminoff <sunnset07@gmail.com>
2024-02-09 17:55:41 -0800

    Trying to implement carousel on home, currently not working

--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,33 @@
 const React = require('react')
 const Default = require('./layouts/default.jsx')
+const Carousel = require('react-bootstrap/Carousel')
 
 const home = () => {
     return (
         <Default>
-            <main>
-                <div>
-                    <img src='/images/rest-rant-home.jpg' alt='Hands holding ipad looking at restaurant reviews'/>
-                </div>
-                <p>A place where you can rave or rant about restaurants!</p>
-            </main>
+            {/* <main> */}
+                <Carousel>
+                    <Carousel.Item>
+                        <img src='/images/rest-rant-home.jpg' alt='Hands holding ipad looking at restaurant reviews'/>
+                        <Carousel.Caption>
+                            <p>Slide 1</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src='/images/Otterly-Delightful-Cafe.jpg' alt='Otter cafe'/>
+                        <Carousel.Caption>
+                            <p>Slide 2</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src='/images/Seoul-Grill-&-Chill.jpg' alt='Korean barbecue'/>
+                        <Carousel.Caption>
+                            <p>Slide 3</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+                {/* <p>A place where you can rave or rant about restaurants!</p>
+            </main> */}
         </Default>
     )
 }
