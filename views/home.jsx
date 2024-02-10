24ee6f1d22db5d154b93b774bece8336a2d20c85
Vanessa Kasminoff <sunnset07@gmail.com>
2024-02-09 23:46:09 -0800

    Added bootstrap carousel on homepage

--- views/home.jsx
+++ views/home.jsx
@@ -1,31 +1,36 @@
 const React = require('react')
 const Default = require('./layouts/default.jsx')
-const Carousel = require('react-bootstrap/Carousel')
 
 const home = () => {
     return (
         <Default>
+            <div id='carouselExampleIndicators' className='carousel slide' data-bs-ride='carousel'>
+                <div className='carousel-indicators'>
+                    <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='0' className='active' aria-current='true' aria-label='Slide 1'></button>
+                    <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='1' aria-label='Slide 2'></button>
+                    <button type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide-to='2' aria-label='Slide 3'></button>
+                </div>
+                <div className='carousel-inner'>
+                    <div className='carousel-item active'>
+                        <img src='/images/rest-rant-home.jpg' className='d-block w-100' alt='Hands holding ipad looking at restaurant reviews'/>
+                    </div>
+                    <div className='carousel-item'>
+                        <img src='/images/Otterly-Delightful-Cafe.jpg' className='d-block w-100' alt='Otter cafe'/>
+                    </div>
+                    <div className='carousel-item'>
+                        <img src='/images/Seoul-Grill-&-Chill.jpg' className='d-block w-100' alt='Korean barbecue'/>
+                    </div>
+                </div>
+                <button className='carousel-control-prev' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide='prev'>
+                    <span className='carousel-control-prev-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Previous</span>
+                </button>
+                <button className='carousel-control-next' type='button' data-bs-target='#carouselExampleIndicators' data-bs-slide='next'>
+                    <span className='carousel-control-next-icon' aria-hidden='true'></span>
+                    <span className='visually-hidden'>Next</span>
+                </button>
+            </div>
             {/* <main> */}
-                <Carousel>
-                    <Carousel.Item>
-                        <img src='/images/rest-rant-home.jpg' alt='Hands holding ipad looking at restaurant reviews'/>
-                        <Carousel.Caption>
-                            <p>Slide 1</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src='/images/Otterly-Delightful-Cafe.jpg' alt='Otter cafe'/>
-                        <Carousel.Caption>
-                            <p>Slide 2</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src='/images/Seoul-Grill-&-Chill.jpg' alt='Korean barbecue'/>
-                        <Carousel.Caption>
-                            <p>Slide 3</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
                 {/* <p>A place where you can rave or rant about restaurants!</p>
             </main> */}
         </Default>
--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -30,6 +30,7 @@ const Default = (html) => {
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" 
                 integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" 
                 crossOrigin="anonymous"></script>
+                <script src='/js/carousel.js'></script>
             </body>
         </html>
     )
